ed163aec2e55a14c6b643afa1627b0f71ff92ac7
조용일 <yedn1128@naver.com>
2022-01-01 18:22:20 +0900

    마이바티스 설정 및 매퍼 경로지정

--- board/src/main/java/board/configuration/DatabaseConfiguration.java
+++ board/src/main/java/board/configuration/DatabaseConfiguration.java
@@ -1,7 +1,12 @@
 package board.configuration;
 import javax.sql.DataSource;
 
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -13,6 +18,9 @@ import com.zaxxer.hikari.HikariDataSource;
 @PropertySource("classpath:/application.properties")
 public class DatabaseConfiguration {
 	
+	@Autowired
+	private ApplicationContext applicationContext;
+	
 	@Bean
 	@ConfigurationProperties(prefix="spring.datasource.hikari")
 	public HikariConfig hikariConfig() {
@@ -21,8 +29,23 @@ public class DatabaseConfiguration {
 	
 	@Bean
 	public DataSource dataSource() throws Exception{
-		DataSource dataSource = new HikariDataSource(hikariConfig());
-		System.out.println(dataSource.toString());
-		return dataSource;
+		return new HikariDataSource(hikariConfig());
+	}
+	
+	@Bean
+	public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception{
+		// SqlSessionFactory를 생성하기 위해 SqlSessionFactoryBea 생성
+		// 마이바티스 단독으로 사용할 경우에는 SqlSessionFactoryBuilder 사
+		SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+		sqlSessionFactoryBean.setDataSource(dataSource);
+		// 매퍼 파일의 위치 설정 . classpath -> resourcess 풀더 의
+		sqlSessionFactoryBean.setMapperLocations(applicationContext.getResources("classpath:/mapper/**/sql-*.xml"));
+		
+		return sqlSessionFactoryBean.getObject();
+	}
+	
+	@Bean
+	public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
+		return new SqlSessionTemplate(sqlSessionFactory);
 	}
 }
