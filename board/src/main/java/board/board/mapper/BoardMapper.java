f5223f4c08b6e97ddeb9a3457440867a9204a746
malvr00 <yedn1128@naver.com>
2022-01-04 01:28:58 +0900

    Detail select 및 조회수 증가 쿼리 추가

--- board/src/main/java/board/board/mapper/BoardMapper.java
+++ board/src/main/java/board/board/mapper/BoardMapper.java
@@ -8,4 +8,6 @@ import board.board.dto.BoardDto;
 public interface BoardMapper {
 	List<BoardDto> selectBoardList() throws Exception;
 	void insertBoard(BoardDto board) throws Exception;
+	void updateHitCount(int boardIdx) throws Exception;
+	BoardDto selectBoardDetail(int boardIdx) throws Exception;
 }
--- board/src/main/java/board/board/service/BoardService.java
+++ board/src/main/java/board/board/service/BoardService.java
@@ -6,4 +6,5 @@ import java.util.List;
 public interface BoardService {
 	List<BoardDto> selectBoardList() throws Exception;
 	void insertBoard(BoardDto board) throws Exception;
+	BoardDto selectBoardDetail(int boardIdx) throws Exception;
 }
