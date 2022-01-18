a71c58bfd77aab15081da9abf167e3d7b6ad0846
malvr00 <yedn1128@naver.com>
2022-01-18 15:49:03 +0900

    예오처리 class 주석 추가

--- board/src/main/java/board/common/ExceptionHandler.java
+++ board/src/main/java/board/common/ExceptionHandler.java
@@ -6,16 +6,16 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.servlet.ModelAndView;
 
-@ControllerAdvice
+@ControllerAdvice	//해당 클래스가 예외처리 클래스임을 알려
 public class ExceptionHandler {
 	private Logger log = LoggerFactory.getLogger(this.getClass());
 	
-	@org.springframework.web.bind.annotation.ExceptionHandler(Exception.class)
+	@org.springframework.web.bind.annotation.ExceptionHandler(Exception.class)	//Exception.class로 모든 예외를 처리함 ( 실제에서는 각가의 예외처리가 필요 )
 	public ModelAndView defaultExceptionHandler(HttpServletRequest request, Exception exception) {
-		ModelAndView mv = new ModelAndView("/error/error_default");
+		ModelAndView mv = new ModelAndView("/error/error_default");	// 예외 발생 시 보여줄 화면 지정
 		mv.addObject("exception" ,exception);
 		
-		log.error("exception", exception);
+		log.error("exception", exception);	// 에러로그 출력
 		
 		return mv;
 	}
