8bc52177824c55a7f16a8c03a14d201027f83f73
malvr00 <yedn1128@naver.com>
2022-01-18 16:32:26 +0900

    파일업로드 라이브리러 다운 및 파일업로드 Bean 설정추가

--- board/src/main/java/board/BoardApplication.java
+++ board/src/main/java/board/BoardApplication.java
@@ -2,8 +2,13 @@ package board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.MultipartAutoConfiguration;
 
-@SpringBootApplication
+// multipartResolver를 등록 했기 때문에 첨부파일과 관련된 자동 구성을 사용하지 않도록 변경.
+// SpringBootApllication은 SpringBootConfiguration, ComponentScan, EnableAutoConfiguration 세개의
+// 어노테이션으로 구성되어 있는데 EnableAutoConfiguration은 스프링 부트의 자동구성을 사용할 때 
+// exclude를 이용해서 특정한 자동구성을 사용하지 않도록 할 수 있다.
+@SpringBootApplication(exclude= {MultipartAutoConfiguration.class})
 public class BoardApplication {
 
 	public static void main(String[] args) {
--- board/src/main/java/board/configuration/WebMvcConfiguration.java
+++ board/src/main/java/board/configuration/WebMvcConfiguration.java
@@ -1,6 +1,8 @@
 package board.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,5 +14,14 @@ public class WebMvcConfiguration implements WebMvcConfigurer{
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(new Loggerinterceptor());
+	}
+	
+	@Bean
+	public CommonsMultipartResolver multipartResolver() {
+		CommonsMultipartResolver commonsMultipartResolver = new CommonsMultipartResolver();
+		commonsMultipartResolver.setDefaultEncoding("UTF-8");	// 인코딩 UTF-8로 설정
+		commonsMultipartResolver.setMaxUploadSizePerFile(5 % 1024 * 1024);	// 파일 크기 설정 (byte)단위로 설정
+		
+		return commonsMultipartResolver;
 	}
 }
