773bd4ade02fdd89e947a87bddf3ea59cce11ff9
malvr00 <yedn1128@naver.com>
2022-01-24 12:42:53 +0900

    RestApi 생성

---
+++ board/src/main/java/board/board/controller/RestBoardApiController.java
@@ -0,0 +1,49 @@
+package board.board.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import board.board.dto.BoardDto;
+import board.board.service.BoardService;
+
+// Controller와 ResponseBody 어노테이션을합친 어노테이션 이다.
+// RestController를 사용하면 해당 API 응답결과를 웹 응답바디를 이요해서 보내준다.
+@RestController
+public class RestBoardApiController {
+	
+	@Autowired
+	private BoardService boardService;
+	
+	@RequestMapping(value="/api/board", method=RequestMethod.GET)
+	public List<BoardDto> openBoardList() throws Exception{
+		return boardService.selectBoardList();
+	}
+	
+	@RequestMapping(value="/api/board/write", method=RequestMethod.POST)
+	public void insertBoard(@RequestBody BoardDto board) throws Exception{
+		boardService.insertBoard(board, null);
+	}
+	
+	@RequestMapping(value="/api/board/{boardIdx}", method=RequestMethod.GET)
+	public BoardDto openBoardDetail(@PathVariable("boardIdx") int boardIdx) throws Exception{
+		return boardService.selectBoardDetail(boardIdx);
+	}
+	
+	@RequestMapping(value="/api/board/{boardIdx}", method=RequestMethod.PUT)
+	public String updateBoard(@RequestBody BoardDto board) throws Exception{
+		boardService.updateBoard(board);
+		return "redirct:/board";
+	}
+	
+	@RequestMapping(value="/api/board/{boardIdx}", method=RequestMethod.DELETE)
+	public String deleteBoard(@PathVariable("boardIdx") int boardIdx) throws Exception{
+		boardService.deleteBoard(boardIdx);
+		return "redirect:/board";
+	}
+}
