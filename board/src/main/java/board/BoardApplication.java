f6a66856ccce24d4e80ddcc419b8ecb27306e8ed
malvr00 <yedn1128@naver.com>
2022-01-25 19:13:09 +0900

    날짜 api tjfwjd ( Jsr310JapConverters 적용 )

--- board/src/main/java/board/BoardApplication.java
+++ board/src/main/java/board/BoardApplication.java
@@ -3,7 +3,15 @@ package board;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.MultipartAutoConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+
+@EnableJpaAuditing
+@EntityScan(
+		basePackageClasses = {Jsr310JpaConverters.class},
+		basePackages= {"board"})
 // multipartResolver를 등록 했기 때문에 첨부파일과 관련된 자동 구성을 사용하지 않도록 변경.
 // SpringBootApllication은 SpringBootConfiguration, ComponentScan, EnableAutoConfiguration 세개의
 // 어노테이션으로 구성되어 있는데 EnableAutoConfiguration은 스프링 부트의 자동구성을 사용할 때 
